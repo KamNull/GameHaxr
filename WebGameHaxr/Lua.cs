116be91b4985c5e1042da8d286dd3e765a17b096
Kamil <kamil@programmer.net>
2017-12-25 13:32:44 +1100

    Create Lua.cs

---
+++ WebGameHaxr/Lua.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebGameHaxr
+{
+    // [Source]
+    class Lua
+    {
+        LuaCore luaCore = new LuaCore();
+
+        public int Execute(string luaCode)
+        {
+            return luaCore.Execute(luaCode);
+        }
+
+        public bool Bool(string luaConditons)
+        {
+            return luaCore.Bool(luaConditons);
+        }
+
+        #region Lua functions for JS
+
+        public void ShowMsg(string luaMessage)
+        {
+            luaCore.Execute("showMessage(\"" + luaMessage + "\")");
+        }
+
+        public int ScanAob(string luaVarAOB, string aob)
+        {
+            luaCore.Execute(luaVarAOB + " = AOBScan(\"" + aob + "\")");
+
+            if (luaCore.Bool(luaVarAOB + " == Nil"))
+                return 0;
+
+            return luaCore.Execute("return " + luaVarAOB + ".Count");
+        }
+
+        public int ReplaceAobResults(string luaVarAOB, string replaceWith, string writeFunc)
+        {
+            if (luaCore.Bool(luaVarAOB + " == Nil"))
+            {
+                return 0;
+            }
+
+            luaCore.Execute("for i=0," + luaVarAOB + ".Count-1 do " +
+                            writeFunc + "(" + luaVarAOB + "[i], " + replaceWith + ") " +
+                        "end");
+
+            return luaCore.Execute("return " + luaVarAOB + ".Count");
+        }
+
+        #endregion
+
+    }
+}
