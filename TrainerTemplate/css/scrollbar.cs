a6bfb4b95dede6323eac33bb7b6ad2714c409059
Kamil <kamil@programmer.net>
2018-03-13 01:56:19 +1100

    Create scrollbar.cs

---
+++ TrainerTemplate/css/scrollbar.cs
@@ -0,0 +1,41 @@
+::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+}
+
+::-webkit-scrollbar-button {
+    width: 0px;
+    height: 0px;
+}
+
+::-webkit-scrollbar-thumb {
+    background: #a0a0a0;
+    border: 0px none #ffffff;
+    border-radius: 50px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #e0e0e0;
+}
+
+::-webkit-scrollbar-thumb:active {
+    background: #4b5158;
+}
+
+::-webkit-scrollbar-track {
+    background: #666666;
+    border: 0px none #ffffff;
+    border-radius: 50px;
+}
+
+::-webkit-scrollbar-track:hover {
+    background: #666666;
+}
+
+::-webkit-scrollbar-track:active {
+    background: #333333;
+}
+
+::-webkit-scrollbar-corner {
+    background: transparent;
+}
