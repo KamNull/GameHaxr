f327b27a6bd2a229a288d057926811fa9fd0c2d8
Kamil <kamil@programmer.net>
2018-03-12 22:02:49 +1100

    Create main.js

---
+++ Library/LuaFileTalk/main.js
@@ -0,0 +1,16 @@
+class LuaFileTalk {
+    constructor(fileDir, mainDir) {
+        this.fileDir = fileDir;
+        lua.setVar('luaServerPath', '"' + fileDir.replace(/\\/g, '\\\\') + '"');
+        this.mainDir = mainDir;
+
+        // Add main class
+        lua.execute(engineIO.fileReadText(this.mainDir + 'main.lua'));
+    }
+
+    eval(luaCode) {
+        lua.execute('returnValue([[ ' + luaCode + ' ]])')
+        return engineIO.fileReadText(this.fileDir + 'result.txt');
+    }
+
+}
