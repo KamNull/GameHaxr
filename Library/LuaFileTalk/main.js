8de483b377deb3a16c97e49bd3aa36e78d528220
Kamil <kamil@programmer.net>
2018-03-13 13:23:23 +1100

    Added parseVar and checkForJS

--- Library/LuaFileTalk/main.js
+++ Library/LuaFileTalk/main.js
@@ -1,20 +1,55 @@
 class LuaFileTalk {
-    constructor(fileDir, mainDir) {
+    constructor(fileDir, mainDir, refreshRate = 100) {
+        // Parse string values into actual type
+        this.autoParse = true;
+        // Set file directory
         this.fileDir = fileDir;
         lua.setVar('luaServerPath', '"' + fileDir.replace(/\\/g, '\\\\') + '"');
         this.mainDir = mainDir;
 
         // Add main class
         lua.execute(engineIO.fileReadText(this.mainDir + 'main.lua'));
+
+        if (refreshRate > 0) {
+            // Check for JS requests from Lua
+            setInterval(this.checkForJS, refreshRate, this.fileDir);
+        }
     }
 
     execute(luaCode) {
-        lua.execute('returnValue([[ ' + luaCode + ' ]])')
-        return engineIO.fileReadText(this.fileDir + 'result.txt');
+        lua.execute('returnValue([[' + luaCode + ']])')
+        if (!this.autoParse)
+            return engineIO.fileReadText(this.fileDir + 'result.txt');
+        else
+            return this.parseVar(engineIO.fileReadText(this.fileDir + 'result.txt'));
     }
 
     getVar(luaVar) {
         return this.execute('return ' + luaVar);
+    }
+
+    parseVar(luaVarStr) {
+        if (luaVarStr.toLowerCase() == 'nil') return null;
+        // Array
+        if (luaVarStr.startsWith('{') && luaVarStr.endsWith('}')) {
+            // Remove { } and put [ ]
+            return window.eval('[' + luaVarStr.slice(1, -1) + ']');
+        }
+
+        // Any other value
+        return window.eval(luaVarStr);
+    }
+
+    checkForJS(fileDir) {
+        if (lua.bool('requestedJS')) {
+            // Avoid duplicate calls
+            lua.setVar('requestedJS', 'false');
+            // Execute the JavaScript
+            console.info('Recieved JavaScript request from Lua.');
+            window.eval(engineIO.fileReadText(fileDir + 'javascript.txt'));
+            return true;
+        }
+        return false;
     }
 
 }
